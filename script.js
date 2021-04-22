7caa3345ca1b88b3fca916769e589d3c4bed869b
Shasi Kanth Dulipalla <shasi.dulipalla@valmont.com>
2021-04-23 01:12:34 +0530

    Introduction to HTML

---
+++ script.js
@@ -0,0 +1,20 @@
+function submitForm() {
+  if (document.getElementById("username").value == "") {
+    alert("Please enter name");
+    document.getElementById("username").focus();
+    return false;
+  }
+  return true;
+}
+
+function changeName() {
+  document.getElementById("name").value = "Suresh";
+}
+
+function changeParagraphText() {
+  document.getElementById("helloworld").innerHTML = "Hello JavaScript";
+}
+
+function onchange_mobile(mobile_number) {
+  console.log(mobile_number);
+}
