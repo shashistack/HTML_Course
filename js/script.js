aa2e5bb5469f6fdd5577c289ae537b959e7306b2
Shasi Kanth Dulipalla <shasi.dulipalla@valmont.com>
2021-04-29 09:19:37 +0530

    added form validations

--- js/script.js
+++ js/script.js
@@ -5,9 +5,42 @@ function submitForm() {
     document.getElementById("username").focus();
     return false;
   }
+  if (document.getElementById("mobile_no").value.length != 10) {
+    alert("Mobile number should be 10 digits length");
+    document.getElementById("mobile_no").focus();
+    return false;
+  }
   if (document.getElementById("city").value == "") {
     alert("Please enter city");
     document.getElementById("city").focus();
+    return false;
+  }
+  if (
+    !document.getElementById("vehicle1").checked &&
+    !document.getElementById("vehicle2").checked &&
+    !document.getElementById("vehicle3").checked
+  ) {
+    alert("Please check at least one option");
+    return false;
+  }
+  if (document.getElementById("biography").value == "") {
+    alert("Please enter your biography");
+    return false;
+  }
+  if (document.getElementById("lang_dropdown").value == "") {
+    alert("Please select the language");
+    return false;
+  }
+  if (document.getElementById("lang_dropdown").value == "") {
+    alert("Please select the language");
+    return false;
+  }
+  if (
+    !document.getElementById("tamil").checked &&
+    !document.getElementById("kannada").checked &&
+    !document.getElementById("malayalam").checked
+  ) {
+    alert("Please select a language");
     return false;
   }
   return true;
